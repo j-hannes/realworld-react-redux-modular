1c21476d245c6c62d8f76d7c6e766c06216214d9
j-hannes <hannes_e@gmx.de>
2017-05-09 21:57:48 +0200

    Fix types

--- src/modules/articles/types.js
+++ src/modules/articles/types.js
@@ -1,13 +1,15 @@
 import ImmutablePropTypes from 'react-immutable-proptypes'
 import PropTypes from 'prop-types'
 
+export const Author = ImmutablePropTypes.recordOf({
+  image: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+})
+
 export const Article = ImmutablePropTypes.recordOf({
   title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  author: ImmutablePropTypes.recordOf({
-    image: PropTypes.string.isRequired,
-    username: PropTypes.string.isRequired,
-  }).isRequired,
+  description: PropTypes.string,
+  author: Author.isRequired,
   createdAt: PropTypes.string.isRequired,
   favoritesCount: PropTypes.number.isRequired,
 })
